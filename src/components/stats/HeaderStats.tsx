77dd3bc2e8be4cc33678c1e96406940058ae79cc
ManonTfx <manon.trefoux@gmail.com>
2022-09-01 12:27:34 +0200

    inverse stats global and user

--- src/components/stats/HeaderStats.tsx
+++ src/components/stats/HeaderStats.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Dispatch, SetStateAction, useContext } from 'react';
 import { DarkModeContext } from '../../context/darkModeContext';
 
@@ -12,22 +13,20 @@ function HeaderStats({ setIsMyStats, isMyStats }: IProps): JSX.Element {
   return (
     <div style={{ backgroundColor }} className="pt-4 pl-4 ">
       <button
-        className={` ${
-          isMyStats ? ' border-b border-primary border-b-2' : ''
-        } mr-4 pb-2`}
-        type="button"
-        onClick={() => setIsMyStats(true)}
-      >
-        Mes stats
-      </button>
-      <button
-        className={` ${
-          !isMyStats ? ' border-b border-primary border-b-2' : ''
-        } pb-2`}
+        className={` ${!isMyStats ? ' border-b border-primary border-b-2' : ''
+          } mr-4 pb-2`}
         type="button"
         onClick={() => setIsMyStats(false)}
       >
         Stats globales
+      </button>
+      <button
+        className={` ${isMyStats ? ' border-b border-primary border-b-2' : ''
+          } pb-2`}
+        type="button"
+        onClick={() => setIsMyStats(true)}
+      >
+        Mes stats
       </button>
     </div>
   );
